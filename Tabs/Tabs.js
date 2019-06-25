2c122f5ff07c777d47364e92496e970dc1b6ce9f
May Ng <mngmay@gmail.com>
2019-06-24 17:49:46 -0700

    Added Tabs.js TabLink class.  Tabs now click and display relevant content.

---
+++ Tabs/Tabs.js
@@ -0,0 +1,26 @@
+
+class TabLink{
+    constructor(element){
+      this.element = element;
+      this.data = this.element.dataset.tab;
+      this.content = document.querySelector(`.content[data-tab='${this.data}']`);
+
+      this.element.addEventListener('click', () => this.selectContent());
+    }
+    
+    selectContent(){
+      const contentList = document.querySelectorAll(".content");
+      contentList.forEach(content => content.classList.remove("showup"));
+      this.content.classList.toggle("showup");
+
+      const linkList = document.querySelectorAll(".link");
+      linkList.forEach(link => link.classList.remove("link-highlight"));
+      this.element.classList.toggle("link-highlight");
+      
+    }
+  }
+
+
+const links = document.querySelectorAll(".link");
+
+links.forEach(link=> new TabLink(link));
