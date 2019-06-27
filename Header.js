ca43d46dac2d4da4145a3cd74a156818c7866603
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-06-27 19:25:56 -0400

    added mouse over and mouse out to a tags - works perfectly yay

--- Header.js
+++ Header.js
@@ -57,3 +57,122 @@ fourth.addEventListener('mouseover', function(event) {
 fourth.addEventListener('mouseout', function(event) {
     fourth.style.border = 'none';
 });
+
+let ab = document.querySelector('.ab');
+ab.addEventListener('mouseover', function(event) {
+    ab.style.color = 'slategray';
+});
+
+ab.addEventListener('mouseout', function(event) {
+    ab.style.color = 'black';
+});
+
+
+let b = document.querySelector('.b');
+b.addEventListener('mouseover', function(event) {
+    b.style.color = 'slategray';
+});
+
+b.addEventListener('mouseout', function(event) {
+    b.style.color = 'black';
+});
+
+
+let c = document.querySelector('.c');
+c.addEventListener('mouseover', function(event) {
+    c.style.color = 'slategray';
+});
+
+c.addEventListener('mouseout', function(event) {
+    c.style.color = 'black';
+});
+
+
+let d = document.querySelector('.d');
+d.addEventListener('mouseover', function(event) {
+    d.style.color = 'slategray';
+});
+
+d.addEventListener('mouseout', function(event) {
+    d.style.color = 'black';
+});
+
+
+let e = document.querySelector('.e');
+e.addEventListener('mouseover', function(event) {
+    e.style.color = 'slategray';
+});
+
+e.addEventListener('mouseout', function(event) {
+    e.style.color = 'black';
+});
+
+
+let f = document.querySelector('.f');
+f.addEventListener('mouseover', function(event) {
+    f.style.color = 'slategray';
+});
+
+f.addEventListener('mouseout', function(event) {
+    f.style.color = 'black';
+});
+
+
+let g = document.querySelector('.g');
+g.addEventListener('mouseover', function(event) {
+    g.style.color = 'slategray';
+});
+
+g.addEventListener('mouseout', function(event) {
+    g.style.color = 'black';
+});
+
+
+let h = document.querySelector('.h');
+h.addEventListener('mouseover', function(event) {
+    h.style.color = 'slategray';
+});
+
+h.addEventListener('mouseout', function(event) {
+    h.style.color = 'black';
+});
+
+
+let i = document.querySelector('.i');
+i.addEventListener('mouseover', function(event) {
+    i.style.color = 'slategray';
+});
+
+i.addEventListener('mouseout', function(event) {
+    i.style.color = 'black';
+});
+
+
+let j = document.querySelector('.j');
+j.addEventListener('mouseover', function(event) {
+    j.style.color = 'slategray';
+});
+
+j.addEventListener('mouseout', function(event) {
+    j.style.color = 'black';
+});
+
+
+let k = document.querySelector('.k');
+k.addEventListener('mouseover', function(event) {
+    k.style.color = 'slategray';
+});
+
+k.addEventListener('mouseout', function(event) {
+    k.style.color = 'black';
+});
+
+
+let l = document.querySelector('.l');
+l.addEventListener('mouseover', function(event) {
+    l.style.color = 'slategray';
+});
+
+l.addEventListener('mouseout', function(event) {
+    l.style.color = 'black';
+});
