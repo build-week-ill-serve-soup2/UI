e184dacb392bbc358064aea67e4d2f4312c52a67
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-06-27 18:17:42 -0400

    fixed the mouseover for bottom content

--- Header.js
+++ Header.js
@@ -1,10 +1,38 @@
-// let navColor = document.querySelector('.first');
-// navColor.addEventListener('mouseover', function(event) {
-//     event.target.style.border = '2px solid black';
-//     event.stopPropagation();
-// });
+let navColor = document.querySelector('.first');
+navColor.addEventListener('mouseover', function(event) {
 
-// navColor.addEventListener('mouseout', function(event) {
-//     event.target.style.color = '@main';
-//     event.target.style.border = '1px solid @main';
-// });
+    navColor.style.border = '2px solid black';
+
+});
+
+navColor.addEventListener('mouseout', function(event) {
+
+    navColor.style.border = 'none';
+});
+
+let second = document.querySelector('.second');
+second.addEventListener('mouseover', function(event) {
+    second.style.border = '2px solid black';
+});
+
+second.addEventListener('mouseout', function(event) {
+    second.style.border = 'none';
+});
+
+let third = document.querySelector('.third');
+third.addEventListener('mouseover', function(event) {
+    third.style.border = '2px solid black';
+});
+
+third.addEventListener('mouseout', function(event) {
+    third.style.border = 'none';
+});
+
+let fourth = document.querySelector('.fourth');
+fourth.addEventListener('mouseover', function(event) {
+    fourth.style.border = '2px solid black';
+});
+
+fourth.addEventListener('mouseout', function(event) {
+    fourth.style.border = 'none';
+});
