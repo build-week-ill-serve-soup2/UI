c6d9a554f69b2bb778babbf194cb16cbd4fe641a
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-06-27 11:38:54 -0400

    fixing accordion bug

--- Header.js
+++ Header.js
@@ -1,5 +1,11 @@
-let navColor = document.querySelector('.hover');
-navColor.addEventListener('mouseover', function(e) {
-    event.target.style.color = 'black';
-    event.target.style.border = '1px solid @main';
-});
+// let navColor = document.querySelector('.hover');
+// navColor.addEventListener('mouseover', function(e) {
+//     event.target.style.color = 'black';
+//     event.target.style.border = '1px solid @main';
+// });
+
+// let navColor = document.querySelector('.hover');
+// navColor.addEventListener('mouseout', function(e) {
+//     event.target.style.color = '@main';
+//     event.target.style.border = '1px solid @main';
+// });
